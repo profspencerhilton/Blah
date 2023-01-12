4dfc4b7c8f7fac0b3caa93c047b229f82112b0c0
Spencer <Spencer@DESKTOP-6CNDRCG>
2023-01-12 12:33:07 -0700

    Add project files.

---
+++ Blah/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
